e90a31cb39b04cf84e523e99756ab043859df373
Gianni Huynh <gianni.huynh@protonmail.com>
2021-09-30 10:54:12 +0200

    Ajout des commentaires

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
-//Gianni HUYNH 2021
+// Fichier JavaScript de Crusher
+// Créé par Gianni HUYNH 2021 ©
 
-let esprit;
-let physique;
-let fidelite;
-let sexuelle;
-let meName;
-let meFirstName;
-let crushName;
-let crushFirstName;
+
+
+// On commence par déclarer toutes nos variables importantes
+let esprit;     // <- Contient la valeur d'esprit (entre 0 et 4)
+let physique;   // <- Contient la valeur physique (entre 0 et 4)
+let fidelite;   // <- Contient la valeur de fidélité (entre 0 et 4)
+let sexuelle;   // <- Contient la valeur sexuelle (entre 0 et 4)
+let meName;     // <- Nom de l'utilisateur
+let meFirstName; // <- Prénom de l'utilisateur
+let crushName;  // <- Nom du Crush
+let crushFirstName; // <- Prénom du Crush
+
+
+
+// Fonction permettant de générer un nombre aléatoire entre un interval donné !
 
 function nbAleatoire(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function launchCrush() {
-  // On remet les cercles en blanc
 
+// Fonction de lancement de calcul de compatibilité
+function launchCrush() {
+  
+  // On restore tout les ronds en Blanc dès le début
   document.getElementById("EUN").style.backgroundColor = "white";
   document.getElementById("EDEUX").style.backgroundColor = "white";
   document.getElementById("ETROIS").style.backgroundColor = "white";
@@ -33,19 +43,20 @@ function launchCrush() {
   document.getElementById("STROIS").style.backgroundColor = "white";
   document.getElementById("SQUATRE").style.backgroundColor = "white";
 
-  //Partie STATS
-
-
+  // On vérifie que aucuns champs ne soit vide !
   if (document.getElementById("nomMe").value != "" && document.getElementById("prenomMe").value != "" 
   && document.getElementById("ageMe").value != "" &&   document.getElementById("astroMe").value != "" 
   && document.getElementById("nomCrush").value != "" &&  document.getElementById("prenomCrush").value != "" &&
   document.getElementById("ageCrush").value != "" && document.getElementById("astroCrush").value != ""){
 
-    esprit = nbAleatoire(0, 4);
+
+  // On lance le calcul des scores de compatibilité, c'est totalement random mais chuut ;)
+  esprit = nbAleatoire(0, 4);
   physique = nbAleatoire(0, 4);
   fidelite = nbAleatoire(0, 4);
   sexuelle = nbAleatoire(0, 4);
 
+  // On colorie les cercles en accord avec les scores obtenus
   if (esprit == 1) {
     document.getElementById("EUN").style.backgroundColor = "#ed147d";
   }
@@ -134,11 +145,14 @@ function launchCrush() {
     document.getElementById("SQUATRE").style.backgroundColor = "#ed147d";
   }
 
+  // On récupère les informations entrées précédement par l'utilisateur
   meName = document.getElementById("nomMe").value;
   meFirstName = document.getElementById("prenomMe").value;
   crushName = document.getElementById("nomCrush").value;
   crushFirstName = document.getElementById("prenomCrush").value;
 
+
+  //On affiche le message avec le nom de l'utilisateur et des crush.
   Resultat.innerHTML =
   meFirstName +
   " " +
@@ -149,6 +163,7 @@ function launchCrush() {
   crushName +
   " ! ✨ Voici les résultats obtenus : ";
 
+  // Et on vide les champs de textes !
   document.getElementById("nomMe").value = "";
   document.getElementById("prenomMe").value = "";
   document.getElementById("ageMe").value = "";
@@ -162,6 +177,8 @@ function launchCrush() {
 
   }
 
+
+  // Si tout les champs  ne sont pas remplis 
   else {
     Resultat.innerHTML = "Opération impossible, veuillez remplir tout les champs afin de calculer les statistiques de compatibilité";
 
